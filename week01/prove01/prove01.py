2d00fda832f7a9f968f2e628752e8483d89bd565
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-09-20 16:11:05 -0600

    Add user interface to select dataset

--- week01/prove01/prove01.py
+++ week01/prove01/prove01.py
@@ -2,38 +2,6 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-iris = datasets.load_iris()
-
-# Show the data (the attributes of each instance)
-# print("\nDATA:")
-# print(iris.data)
-data = iris.data
-
-# Show the target values (in numeric format) of each instance
-# print("\nTARGET:")
-# print(iris.target)
-target = iris.target
-
-# Show the actual target names that correspond to each number
-# print("\nTARGET NAMES:")
-# print(iris.target_names)
-
-# Split the data into a training set and a testing set
-data_train, data_test, targets_train, targets_test = train_test_split(
-    data, target, test_size=0.7, random_state=0)
-
-# Train the model based on the training data
-classifier = GaussianNB()
-classifier.fit(data_train, targets_train)
-
-# Model makes a predicition using what it learned from the classifier
-targets_predicted = classifier.predict(data_test)
-# print(targets_test)
-# print(targets_predicted)
-
-# Get the accuracy of the prediction
-accuracy = accuracy_score(targets_test, targets_predicted) * 100
-print("Accuracy: {}%".format(round(accuracy, 2)))
 
 
 class HardCodedClassifier:
@@ -44,9 +12,78 @@ class HardCodedClassifier:
         return [0 for d in data_test]
 
 
-hc_classifier = HardCodedClassifier()
-hc_classifier.fit(data_train, targets_train)
-hc_pred = hc_classifier.predict(data_test)
+def selectDataset():
+    # Continue to get user input until a return statement is reached
+    while True:
+        user_input = input(
+            "Please enter a dataset to use (or type '-list' to see options)> ")
 
-hc_accuracy = accuracy_score(targets_test, hc_pred) * 100
-print("Accuracy: {}%".format(round(hc_accuracy, 2)))
+        # list all options
+        if user_input == "-list":
+            print("Available datasets are \n'iris'" +
+                  "\n'digits'\n'wine'\n'breast cancer'")
+
+        # Load a classification dataset from sklearn
+        elif user_input == "iris":
+            print(user_input)
+            return datasets.load_iris()
+        elif user_input == "digits":
+            return datasets.load_digits()
+        elif user_input == "wine":
+            return datasets.load_wine()
+        elif user_input == "breast cancer":
+            return datasets.load_breast_cancer()
+
+        else:
+            print("Invalid input. Please try again.")
+
+
+def main(dataset):
+    # Show the data (the attributes of each instance)
+    # print("\nDATA:")
+    # print(dataset.data)
+    data = dataset.data
+
+    # Show the target values (in numeric format) of each instance
+    # print("\nTARGET:")
+    # print(dataset.target)
+    target = dataset.target
+
+    # Show the actual target names that correspond to each number
+    # print("\nTARGET NAMES:")
+    # print(dataset.target_names)
+
+    # Split the data into a training set and a testing set
+    data_train, data_test, targets_train, targets_test = train_test_split(
+        data, target, test_size=0.7)
+
+    # Train the model based on the training data
+    classifier = GaussianNB()
+    classifier.fit(data_train, targets_train)
+
+    # Model makes a predicition using what it learned from the classifier
+    targets_predicted = classifier.predict(data_test)
+    # print(targets_test)
+    # print(targets_predicted)
+
+    # Get the accuracy of the prediction
+    accuracy = accuracy_score(targets_test, targets_predicted)
+    print("Accuracy of Gaussian classifier: {}%".format(
+        round(accuracy * 100, 2)))
+
+    hc_classifier = HardCodedClassifier()
+    hc_classifier.fit(data_train, targets_train)
+    hc_pred = hc_classifier.predict(data_test)
+
+    hc_accuracy = accuracy_score(targets_test, hc_pred)
+    print("Accuracy of HardCoded classifier: {}%".format(
+        round(hc_accuracy * 100, 2)))
+
+
+if __name__ == "__main__":
+    print("USING THE IRIS DATSET: ")
+    main(datasets.load_iris())
+
+    print("\nUSING A USER SELECTED DATASET: ")
+    dataset = selectDataset()
+    main(dataset)
